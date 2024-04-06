4392a3b29998ad88c58ea3a5a5b7b3378b771e4f
Phu Chinyam <phuchinyam@gmail.com>
2024-04-06 14:58:48 -0500

    Added Color picker and cat save data

--- src/main/java/com/example/task_manager/AddTaskController.java
+++ src/main/java/com/example/task_manager/AddTaskController.java
@@ -3,8 +3,11 @@ package com.example.task_manager;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.PickResult;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class  AddTaskController {
@@ -20,12 +23,24 @@ public class  AddTaskController {
     @FXML
     private TextField taskCategoryField;
 
+    @FXML
+    private ColorPicker myColorPicker;
+
     public void saveTask(){
         String taskName = taskNameField.getText();
         String taskDescription = taskDescriptionField.getText();
         Task newTask = new Task(taskName, taskDescription);
+        String categoryName = taskCategoryField.getText();
+        String categoryColor = changeColor();
+        Category newCategory = new Category(categoryName, categoryColor);
         System.out.println(newTask.getName());
+        System.out.println(newCategory.getCategoryColor());
     //database stuff
+    }
+    public String changeColor(){
+        String myColor = myColorPicker.getValue().toString();
+        return myColor;
+
     }
 
 }
