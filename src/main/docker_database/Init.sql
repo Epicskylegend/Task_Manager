aae445f5c7dfd8e079fd0ce96f7454520ef31c32
Epicskylegend <adeba003@gmail.com>
2024-04-13 18:31:58 -0500

    Fixed db issue.

--- src/main/docker_database/Init.sql
+++ src/main/docker_database/Init.sql
@@ -14,18 +14,6 @@ CREATE TABLE IF NOT EXISTS Tasks(
 CREATE TABLE IF NOT EXISTS Categories (
     category_name VARCHAR(255),
     category_color VARCHAR(255)
-    );
-
-
-DO $$
-BEGIN
-
-    IF (SELECT count(*) FROM information_schema.columns WHERE table_name = 'tasks') < 7 THEN
-
-        ALTER TABLE Tasks ADD COLUMN task_completion BOOLEAN;
-        RAISE NOTICE 'task_completion column succesfully added.';
-    END IF;
-END $$;
-
+);
 
 
